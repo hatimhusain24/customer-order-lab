8c9c96f9b48c1a9c9c76cd693d025803adde6b70
Hatim Husain <hatim.husain24@gmail.com>
2026-07-13 00:27:25 +0530

    Initial Spring Boot project setup for customer order lab project

---
+++ src/main/java/com/hatim/customerorderlab/CustomerOrderLabApplication.java
@@ -0,0 +1,13 @@
+package com.hatim.customerorderlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrderLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerOrderLabApplication.class, args);
+    }
+
+}
